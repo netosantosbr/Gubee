0e85d48b2d356eed13adb1b29bc159d6163f03a6
José Alfredo <netoalfredo9@gmail.com>
2022-12-14 14:34:37 -0300

    Dynamic title

--- frontend-api-herois/src/app/views/heroi-crud/heroi-crud.component.ts
+++ frontend-api-herois/src/app/views/heroi-crud/heroi-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-heroi-crud',
@@ -7,7 +8,14 @@ import { Router } from '@angular/router'
   styleUrls: ['./heroi-crud.component.css']
 })
 export class HeroiCrudComponent {
-  constructor(private router: Router){}
+  constructor(private router: Router,
+              private headerService: HeaderService){
+    headerService.headerData = {
+      title: 'Painel de Controle de Heróis',
+      icon: 'sports_martial_arts',
+      routeUrl: '/herois'
+    }
+  }
 
   ngOnInit(): void {}
 
